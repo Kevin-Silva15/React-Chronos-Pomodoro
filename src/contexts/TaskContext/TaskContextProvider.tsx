b0795b214ffedc4c8a4548d6c1733a5992d55a8e
Kevin Silva <kevinsilva.iar21@gmail.com>
2025-09-05 06:22:00 -0300

    Web Worker de TimerWorker finalizado

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,18 +2,50 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workes/TimerWorkerManager";
+import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer( taskReducer, initialTaskState)
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState)
+
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(e => {
+    const countDownSeconds = e.data;
+    // console.log(countDownSeconds);
+
+    if(countDownSeconds <=0){
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      console.log('Worker COMPLETED');
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: {secondsRemaining:countDownSeconds},
+      });
+    }
+
+  });
+
 
   //monitorar o estado, toda vez o estado mudar, assim a gente ve, quando a task ali no console.
   useEffect(() => {
     console.log(state);
-  }, [state]);
+
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+
+  }, [worker, state]);
 
 
   return (
